3f8fa4685cde57e35ef0d070294b41824266499d
RaccoonHsieh <milly861204@gmail.com>
2023-04-06 23:32:49 +0800

    bug修改

--- js/apps.js
+++ js/apps.js
@@ -1,12 +1,4 @@
-$('.nonloop').owlCarousel({
-    center: true,
-    items:2,
-    loop:false,
-    margin:10,
-    responsive:{
-        600:{
-            items:4
-        }
-    }
-});
+$(document).ready(function(){
+    
+})
 
--- js/index.js
+++ js/index.js
@@ -11,11 +11,19 @@ $(window).on('load', function(){
 });
 
 //----------- 首頁第四格 -----------
-$('.map_search').click(function(){
-    $('.bgi').animate({height: '920px'}, 500),
-    $('.location').animate({height: '920px'}, 500),
-    $('.map').animate({opacity: 1}, 700);
-});
+
+    $('.map_search').click(function(){
+    if(viewport > 414){
+        $('.bgi').animate({height: '920px'}, 500),
+        $('.location').animate({height: '920px'}, 500),
+        $('.map').animate({opacity: 1}, 700)
+    }else{
+        $('.bgi').animate({height: '750px'}, 500),
+        $('.location').animate({height: '750px'}, 500),
+        $('.map').animate({opacity: 1}, 700)
+        $('.map').css({height: '350px'})
+    };
+    });
 
 //------ mobile ------
 let viewport = $(window).width();
@@ -30,7 +38,6 @@ $(window).on('load', function(){
             if( burger == false ) {
                 $('.burger').addClass('open'),
                 $('.burger').addClass('openbutton'),
-                // $('.nav_el').css({opacity: 1}),
                 $('.nav_el').slideToggle();
                 burger = true;
             }else{
