54453845eab3f81f40a3c77671e56f324aa82790
RaccoonHsieh <milly861204@gmail.com>
2023-04-08 00:27:15 +0800

    手風琴

--- js/apps.js
+++ js/apps.js
@@ -1,4 +1,27 @@
+let button = false;
+
 $(document).ready(function(){
-    
-})
+    $('.apps-qa-head').click(function(){
+
+        $('.apps-qa-head').removeClass('active');
+        $(this).removeClass('active');
+        $('.apps-qa-content').slideUp();
+
+        if (!$(this).next().is(':visible')) {
+            $(this).next().slideDown();
+            $(this).addClass('active');
+          }
+
+
+        // if( button == false ) {
+        //     $('.apps-qa-content').slideUp();
+        //     $(this).next().slideDown();
+        //     button = true;
+        // }else{
+        //     $(this).next().slideUp();
+        //     button = false;
+        // };
+
+    });
+});
 
